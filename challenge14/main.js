9191c608e00bcca202c0778b756d4828fb62c5cf
michaelcmarafino <michaelcmarafino@gmail.com>
2022-04-13 19:03:09 -0400

    challenge 14

---
+++ challenge14/main.js
@@ -0,0 +1,6 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+// nameShuffler('john McClane'); => "McClane john"
+
+function nameSuffle(str) {
+    return str.split(" ").reverse().join(" ")
+}
