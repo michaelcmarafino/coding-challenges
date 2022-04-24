1ee0c48dc92c57f6f0a7465238ea10b527c3ff67
michaelcmarafino <michaelcmarafino@gmail.com>
2022-04-24 13:30:58 -0400

    challenge 56

---
+++ challenge56/main.js
@@ -0,0 +1,17 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+let reverseSeq = n => {
+    let arr = []
+    for (let i = n; i >= 1; i--) {
+        arr.push(i)
+    }
+    return arr
+}
+
+reverseSeq = n => {
+    return Array(n)
+        .fill(0)
+        .map((e, i) => n - i)
+}
