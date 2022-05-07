8c5a6f3e8c5f4e89bfea3f697ea39eef14f81f0e
michaelcmarafino <michaelcmarafino@gmail.com>
2022-05-06 21:53:08 -0400

    challenge 81

---
+++ challenge81/main.js
@@ -0,0 +1,13 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+    let vowelsCount = 0
+    for (let i = 0; i < str.length; i++) {
+        if ("aeiou".includes(str[i])) vowelsCount++
+    }
+    return vowelsCount
+}
