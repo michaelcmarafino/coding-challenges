9f4fe971bc19e9812166f76d288fe83625091932
michaelcmarafino <michaelcmarafino@gmail.com>
2022-05-28 20:09:53 -0400

    connected to mongodb database

--- crud-example/server.js
+++ crud-example/server.js
@@ -1,14 +1,35 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const MongoClient = require("mongodb").MongoClient
 const app = express()
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
+const connectionString =
+    "mongodb+srv://michaelcmarafino:Rllhh2oZ5tOgnUGx@cluster0.xctf8gr.mongodb.net/?retryWrites=true&w=majority"
 
-app.post("/quotes", (req, res) => {
-    console.log("HELLOOOOO")
-})
+// Database
+MongoClient.connect(connectionString)
+    .then(client => {
+        console.log("Connected to Database")
+        const db = client.db("crud-example")
+        const quotesCollection = db.collection("quotes")
+        // Middleware
+        app.use(bodyParser.urlencoded({ extended: true }))
+        // CRUD handlers
+        app.get("/", (req, res) => {
+            res.sendFile(__dirname + "/index.html")
+        })
 
-app.listen(8000, () => {
-    console.log("Listening on PORT 8000...")
-})
+        app.post("/quotes", (req, res) => {
+            quotesCollection
+                .insertOne(req.body)
+                .then(result => {
+                    res.redirect("/")
+                })
+                .catch(err => console.error(err))
+        })
+
+        app.listen(8000, () => {
+            console.log("Listening on PORT 8000...")
+        })
+    })
+    .catch(err => console.error(err))
