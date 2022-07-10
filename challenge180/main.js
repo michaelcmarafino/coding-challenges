fd373280b8ad4d240f1be43550b1d98a141ffb19
michaelcmarafino <michaelcmarafino@gmail.com>
2022-07-10 17:33:17 -0400

    challenge 180

---
+++ challenge180/main.js
@@ -0,0 +1,34 @@
+// Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motif usually repeats. He isn't fond of seeing the Eiffel tower 40 times.
+// He tells them that he will only sit for the session if they show the same motif at most N times. Luckily, Alice and Bob are able to encode the motif as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+
+// Task
+// Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+// For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+// With list [20,37,20,21] and number 1, the result would be [20,37,21].
+
+function deleteNth(arr, n) {
+    let obj = {}
+    let newArr = []
+    arr.forEach(el => {
+        if (!obj[el]) {
+            obj[el] = 1
+            newArr.push(el)
+        } else {
+            obj[el]++
+            if (obj[el] <= n) {
+                newArr.push(el)
+            }
+        }
+    })
+    return newArr
+}
+
+function deleteNth(arr, n) {
+    let obj = {}
+    return arr.filter(el => {
+        obj[el] = (obj[el] || 0) + 1
+        return obj[el] <= n
+    })
+}
+
+console.log(deleteNth([20, 37, 20, 21, 21, 20, 37, 37, 37], 1))
