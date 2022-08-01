e36d460a9d2b44ec195bb3521ba6d2657239d8e9
michaelcmarafino <michaelcmarafino@gmail.com>
2022-08-01 17:46:49 -0400

    challenge 208

---
+++ challenge208/main.js
@@ -0,0 +1,16 @@
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+
+function replace(s) {
+    return s
+        .split("")
+        .map(el => ("aeiouAEIOU".includes(el) ? "!" : el))
+        .join("")
+}
+
+console.log(replace("Hi!"))
