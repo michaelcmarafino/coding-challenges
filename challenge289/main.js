06992c1b80bdd76c2f4cd12a0b1e0ef9dffaf905
michaelcmarafino <michaelcmarafino@gmail.com>
2022-10-10 18:07:36 -0400

    challenge 289

---
+++ challenge289/main.js
@@ -0,0 +1,11 @@
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+function findLongest(array) {
+    return array.reduceRight((a, b) =>
+        String(a).length > String(b).length ? a : b
+    )
+}
+
+console.log(findLongest([1, 10, 100]))
