0bb55b233a36f35f596152cee5d60cb8b020b437
michaelcmarafino <michaelcmarafino@gmail.com>
2022-11-02 08:30:31 -0400

    challenge 307

---
+++ challenge307/main.js
@@ -0,0 +1,8 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+console.log(findAverage([1, 2, 3]), 2)
+console.log(findAverage([1, 1, 1]), 1)
+console.log(findAverage([1, 2, 3, 4]), 2.5)
+console.log(findAverage([]), 0)
