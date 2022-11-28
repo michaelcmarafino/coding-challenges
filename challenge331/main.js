c0d152ecd867c94c7c21482bbfdead55fd4afce3
michaelcmarafino <michaelcmarafino@gmail.com>
2022-11-28 18:36:12 -0500

    challenge 331

---
+++ challenge331/main.js
@@ -0,0 +1,24 @@
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+
+function dup(s) {
+    return s.map(el =>
+        el
+            .split("")
+            .filter((letter, i, arr) => letter !== arr[i + 1])
+            .join("")
+    )
+}
+
+console.log(dup(["abracadabra", "allottee", "assessee"]), [
+    "abracadabra",
+    "alote",
+    "asese",
+])
