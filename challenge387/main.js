f54cc9dd52025c8b50f3bd97b39a8256587bc638
michaelcmarafino <michaelcmarafino@gmail.com>
2023-02-02 21:45:47 -0500

    challenge 387

---
+++ challenge387/main.js
@@ -0,0 +1,14 @@
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+function mygcd(a, b) {
+    if (b === 0) {
+        return a
+    }
+    return mygcd(b, a % b)
+}
+
+console.log(mygcd(30, 12), 6)
+console.log(mygcd(8, 9), 1)
+console.log(mygcd(1, 1), 1)
