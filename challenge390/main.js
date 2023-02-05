67029739bfc3056902bbe17bb66369059ddaa2a6
michaelcmarafino <michaelcmarafino@gmail.com>
2023-02-04 19:28:39 -0500

    challenge 390

---
+++ challenge390/main.js
@@ -0,0 +1,15 @@
+// You will be given an array of numbers.
+
+// For each number in the array you will need to create an object.
+
+// The object key will be the number, as a string. The value will be the corresponding character code, as a string.
+
+// Return an array of the resulting objects.
+
+// All inputs will be arrays of numbers. All character codes are valid lower case letters. The input array will not be empty.
+
+function numObj(s) {
+    return s.map(el => ({ [el]: String.fromCharCode(el) }))
+}
+
+console.log(numObj([118, 117, 120]), [{ 118: "v" }, { 117: "u" }, { 120: "x" }])
