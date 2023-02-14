9007219204a297b779c31e856161956720bccc04
michaelcmarafino <michaelcmarafino@gmail.com>
2023-02-13 20:14:39 -0600

    challenge 400

---
+++ challenge400/main.js
@@ -0,0 +1,25 @@
+// The Hamming Distance is a measure of similarity between two strings of equal length. Complete the function so that it returns the number of positions where the input strings do not match.
+
+// Examples:
+// a = "I like turtles"
+// b = "I like turkeys"
+// Result: 3
+
+// a = "Hello World"
+// b = "Hello World"
+// Result: 0
+
+// a = "espresso"
+// b = "Expresso"
+// Result: 2
+
+function hamming(a, b) {
+    let bigger = a.length > b.length ? a.length : b.length
+    let total = 0
+    for (let i = 0; i < bigger; i++) {
+        if (a[i] == b[i]) {
+            total++
+        }
+    }
+    return total
+}
