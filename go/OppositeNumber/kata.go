7038366fb5400a314ece2a832133dd5f50908de5
michaelcmarafino <michaelcmarafino@gmail.com>
2024-03-09 10:22:51 -0500

    Opposite number

---
+++ go/OppositeNumber/kata.go
@@ -0,0 +1,13 @@
+//Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+//Examples:
+
+//1: -1
+//14: -14
+//-34: 34
+
+package kata
+
+func Opposite(value int) int {
+	return -value
+}
