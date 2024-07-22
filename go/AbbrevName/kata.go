85d583fbafeb206a76e2186cc2f8407d4f24cd78
michaelcmarafino <michaelcmarafino@gmail.com>
2024-07-22 15:09:40 -0400

    Updated so works with Unicode strings, using runes

--- go/AbbrevName/kata.go
+++ go/AbbrevName/kata.go
@@ -4,14 +4,24 @@ import "strings"
 
 func AbbrevName(name string) string {
 
-	b := []byte(name)
-	firstInitial := b[0]
-	var secondInitialIndex int
-	for i, v := range b {
-		if v == ' ' {
-			secondInitialIndex = i
-		}
+	//b := []byte(name)
+	//firstInitial := b[0]
+	//var secondInitialIndex int
+	//for i, v := range b {
+	//if v == ' ' {
+	//secondInitialIndex = i
+	//}
+	//}
+	//secondInitial := b[secondInitialIndex+1]
+	//return strings.ToUpper(string(firstInitial)) + "." + strings.ToUpper(string(secondInitial))
+
+	//words := strings.Fields(name)
+	//return strings.ToUpper(string(words[0][0])) + "." + strings.ToUpper(string(words[1][0]))
+
+	firstLetterToUpper := func(str string) string {
+		return strings.ToUpper(string([]rune(str)[0]))
 	}
-	secondInitial := b[secondInitialIndex+1]
-	return strings.ToUpper(string(firstInitial)) + "." + strings.ToUpper(string(secondInitial))
+
+	var names = strings.Fields(name)
+	return firstLetterToUpper(names[0]) + "." + firstLetterToUpper(names[1])
 }
