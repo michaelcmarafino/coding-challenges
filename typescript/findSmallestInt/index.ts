dc0adc53467b146976a60c237f91d85e3159b5dc
michaelcmarafino <michaelcmarafino@gmail.com>
2024-09-28 17:17:55 -0400

    Find Smallest Integer

---
+++ typescript/findSmallestInt/index.ts
@@ -0,0 +1,11 @@
+export function findSmallestInt(args: number[]): number {
+    //let result: number = args[0]
+    //for (let a of args) {
+    //if (a < result) {
+    //result = a
+    //}
+    //}
+    //return result
+
+    return Math.min(...args)
+}
